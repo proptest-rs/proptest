f80c1e6e8d8a0527f673e922d25c106ded124543
cameron <cameron.studdstreet@gmail.com>
2025-09-04 05:39:25 +0100

    fix shorthand struct initialization lint

--- proptest-macro/src/property_test/codegen/test_body.rs
+++ proptest-macro/src/property_test/codegen/test_body.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse2, Block, Expr, Ident, ReturnType, Type, TypeTuple};
+use syn::{parse2, Block, Expr, Ident, Pat, ReturnType, Type, TypeTuple};
 
 use crate::property_test::{options::Options, utils::Argument};
 
@@ -22,9 +22,17 @@ pub(super) fn body(
 
     // convert each arg to `field0: x`
     let struct_fields = args.iter().enumerate().map(|(index, arg)| {
-        let pat = &arg.pat_ty.pat;
+        let pat = arg.pat_ty.pat.as_ref();
         let field_name = nth_field_name(args, index);
-        quote!(#field_name: #pat,)
+
+        // If the pattern is an ident, we know that the field name is equal to the pattern name.
+        // This means we need to avoid generating: `x: x`, which would trigger a lint suggesting
+        // shorthand struct initialization.
+
+        match pat {
+            Pat::Ident(_) => quote!(#field_name,),
+            _ => quote!(#field_name: #pat,),
+        }
     });
 
     // e.g. FooArgs { field0: x, field1: (y, z), }
